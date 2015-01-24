4ab04998f1a3125819bca2b3e4f02233963abc7e
Rave-Yori-Kvitchko <yori.kvitchko@ravesocial.co>
2015-01-24 00:07:57 -0800

    In progress commit

---
+++ Assets/Scripts/ChainGenerator.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChainGenerator:MonoBehaviour {
+	public Player[] players;
+	public GameObject chainLinkPrefab;
+
+	private const float PLAYER_DISTANCE = 10f;
+	private const int NUM_LINKS = 10;
+
+	public void Awake() {
+		// Reposition players
+		for(int i = 1; i < players.Length; i++) {
+			players[i].transform.Translate(Vector3.right * PLAYER_DISTANCE * i);
+		}
+
+		// Create prefabs for chain between players
+		for(int i = 1; i < players.Length; i++) {
+			CreateChain(i, players[i-1], players[i]);
+		}
+	}
+
+	public void CreateChain(int playerInd, Player p1, Player p2) {
+		Vector2 linkSize = ((BoxCollider2D) chainLinkPrefab.collider2D).size;
+		float linkOffset = linkSize.x * 0.5f;
+		Vector3 pos = p1.transform.position + (Vector3.right * linkOffset);
+
+		GameObject link;
+		HingeJoint2D hinge;
+		Rigidbody2D prevLink = p1.GetComponent<Rigidbody2D>();
+
+		for(int i = 0; i < NUM_LINKS; i++) {
+			link = (GameObject) GameObject.Instantiate(chainLinkPrefab, pos + Vector3.right * linkSize.x * i, Quaternion.identity);
+			link.name = "ChainP" + playerInd + "_" + i;
+			// Connect current link with previous link (or player)
+			hinge = link.AddComponent<HingeJoint2D>();
+			hinge.connectedBody = prevLink;
+			hinge.anchor = new Vector2(-linkOffset, 0f);
+			hinge.connectedAnchor = new Vector2(linkOffset, 0f);
+			// Set previous link to current link
+			prevLink = link.GetComponent<Rigidbody2D>();
+		}
+
+		// Final hinge
+		hinge = prevLink.gameObject.AddComponent<HingeJoint2D>();
+		hinge.connectedBody = p2.GetComponent<Rigidbody2D>();
+		hinge.anchor = new Vector2(linkOffset, 0f);
+		hinge.connectedAnchor = new Vector2(-linkOffset, 0f);
+	}
+}
---
+++ Assets/Scripts/WallGenerator.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WallGenerator:MonoBehaviour {
+	public GameObject wallPrefab;
+
+	private const float SPAWN_TIME = 2f;
+	private float spawnTimer = 0f;
+	
+	public void Update() {
+		// Timer
+		spawnTimer -= Time.deltaTime;
+
+		if(spawnTimer <= 0f) {
+			// Spawn!
+			SpawnWall();
+
+			spawnTimer = SPAWN_TIME;
+		}
+	}
+
+	public void SpawnWall() {
+		Vector3 pos = new Vector3(Random.Range(0, 22) * 4f, -3f);
+		GameObject wall;
+
+		for(int i = 0; i < Random.Range(3, 10); i++) {
+			wall = (GameObject) GameObject.Instantiate(wallPrefab, pos, Quaternion.identity);
+			wall.name = wallPrefab.name;
+			pos.x += 4f;
+		}
+	}
+}
